cffe9402c63c2881f8067228dbb4e78c094d327a
Ivan Consalter <ivanconsalter1@gmail.com>
2023-04-01 11:58:50 -0300

    project created

---
+++ src/main/java/com/ivanconsalter/ionicspring/IonicSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ivanconsalter.ionicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IonicSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IonicSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivanconsalter/ionicspring/IonicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanconsalter.ionicspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IonicSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
