574e39e3441b04f06df16670323115de56eba767
Ivan Consalter <ivanconsalter1@gmail.com>
2023-04-08 12:32:50 -0300

    update using only @JsonIgnore

--- src/main/java/com/ivanconsalter/ionicspring/domain/Pedido.java
+++ src/main/java/com/ivanconsalter/ionicspring/domain/Pedido.java
@@ -17,7 +17,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable {
@@ -35,12 +34,10 @@ public class Pedido implements Serializable {
 	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 	
-	@JsonManagedReference
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 	
