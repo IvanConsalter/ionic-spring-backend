adef8a7f6ce5fcfbb689fa853e19f186c901d198
Ivan Consalter <ivanconsalter1@gmail.com>
2023-04-19 20:00:29 -0300

    added dev profile

---
+++ src/main/java/com/ivanconsalter/ionicspring/config/DevConfig.java
@@ -0,0 +1,29 @@
+package com.ivanconsalter.ionicspring.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.ivanconsalter.ionicspring.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	public boolean instantiateDatabase() {
+		if(!strategy.equalsIgnoreCase("create")) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+}
