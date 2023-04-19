356366dfb4f28d112aa5128c9f26cc1682de393c
Ivan Consalter <ivanconsalter1@gmail.com>
2023-04-19 19:44:50 -0300

    added test profile

--- src/main/java/com/ivanconsalter/ionicspring/IonicSpringApplication.java
+++ src/main/java/com/ivanconsalter/ionicspring/IonicSpringApplication.java
@@ -1,172 +1,18 @@
 package com.ivanconsalter.ionicspring;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ivanconsalter.ionicspring.domain.Categoria;
-import com.ivanconsalter.ionicspring.domain.Cidade;
-import com.ivanconsalter.ionicspring.domain.Cliente;
-import com.ivanconsalter.ionicspring.domain.Endereco;
-import com.ivanconsalter.ionicspring.domain.Estado;
-import com.ivanconsalter.ionicspring.domain.ItemPedido;
-import com.ivanconsalter.ionicspring.domain.Pagamento;
-import com.ivanconsalter.ionicspring.domain.PagamentoComBoleto;
-import com.ivanconsalter.ionicspring.domain.PagamentoComCartao;
-import com.ivanconsalter.ionicspring.domain.Pedido;
-import com.ivanconsalter.ionicspring.domain.Produto;
-import com.ivanconsalter.ionicspring.domain.enums.EstadoPagamento;
-import com.ivanconsalter.ionicspring.domain.enums.TipoCliente;
-import com.ivanconsalter.ionicspring.repositories.CategoriaRepository;
-import com.ivanconsalter.ionicspring.repositories.CidadeRepository;
-import com.ivanconsalter.ionicspring.repositories.ClienteRepository;
-import com.ivanconsalter.ionicspring.repositories.EnderecoRepository;
-import com.ivanconsalter.ionicspring.repositories.EstadoRepository;
-import com.ivanconsalter.ionicspring.repositories.ItemPedidoRepository;
-import com.ivanconsalter.ionicspring.repositories.PagamentoRepository;
-import com.ivanconsalter.ionicspring.repositories.PedidoRepository;
-import com.ivanconsalter.ionicspring.repositories.ProdutoRepository;
-
 @SpringBootApplication
 public class IonicSpringApplication implements CommandLineRunner {
 	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	
-	@Autowired
-	private ProdutoRepository produtoRepository;
-	
-	@Autowired
-	private EstadoRepository estadoRepository;
-	
-	@Autowired
-	private CidadeRepository cidadeRepository;
-	
-	@Autowired
-	private EnderecoRepository enderecoRepository;
-	
-	@Autowired
-	private ClienteRepository clienteRepository;
-	
-	@Autowired
-	private PedidoRepository pedidoRepository;
-	
-	@Autowired
-	private PagamentoRepository pagamentoRepository;
-	
-	@Autowired
-	private ItemPedidoRepository itemPedidoRepository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(IonicSpringApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
-		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
-		Categoria cat4 = new Categoria(null, "Eletrônicos");
-		Categoria cat5 = new Categoria(null, "Jardinagem");
-		Categoria cat6 = new Categoria(null, "Decoração");
-		Categoria cat7 = new Categoria(null, "Perfumaria");
-		
-		Produto p1 = new Produto(null, "Computador", 2000.00);
-		Produto p2 = new Produto(null, "Impressora", 800.00);
-		Produto p3 = new Produto(null, "Mouse", 80.00);
-		Produto p4 = new Produto(null, "Mesa de escritório", 300.00);
-		Produto p5 = new Produto(null, "Toalha", 50.00);
-		Produto p6 = new Produto(null, "Colcha", 200.00);
-		Produto p7 = new Produto(null, "TV true color", 1200.00);
-		Produto p8 = new Produto(null, "Roçadeira", 800.00);
-		Produto p9 = new Produto(null, "Abajour", 100.00);
-		Produto p10 = new Produto(null, "Pendente", 180.00);
-		Produto p11 = new Produto(null, "Shampoo", 90.00);
-
-		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2));
-		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
-		cat3.getProdutos().addAll(Arrays.asList(p5, p6));
-		cat4.getProdutos().addAll(Arrays.asList(p1, p2, p3, p7));
-		cat5.getProdutos().addAll(Arrays.asList(p8));
-		cat6.getProdutos().addAll(Arrays.asList(p9, p10));
-		cat7.getProdutos().addAll(Arrays.asList(p11));
-		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
-		
-		p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2, cat4));
-		p3.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		p4.getCategorias().addAll(Arrays.asList(cat2));
-		p5.getCategorias().addAll(Arrays.asList(cat3));
-		p6.getCategorias().addAll(Arrays.asList(cat3));
-		p7.getCategorias().addAll(Arrays.asList(cat4));
-		p8.getCategorias().addAll(Arrays.asList(cat5));
-		p9.getCategorias().addAll(Arrays.asList(cat6));
-		p10.getCategorias().addAll(Arrays.asList(cat6));
-		p11.getCategorias().addAll(Arrays.asList(cat7));
-		
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
-		
-		Estado estado1 = new Estado(null, "Minas Gerais");
-		Estado estado2 = new Estado(null, "São Paulo");
-		
-		Cidade cidade1 = new Cidade(null, "Uberlândia", estado1);
-		Cidade cidade2 = new Cidade(null, "Belo Horizonte", estado1);
-		Cidade cidade3 = new Cidade(null, "São Paulo", estado2);
-		Cidade cidade4 = new Cidade(null, "Campinas", estado2);
-		
-		estado1.getCidades().add(cidade1);
-		estado1.getCidades().add(cidade2);
-		
-		estado2.getCidades().add(cidade3);
-		estado2.getCidades().add(cidade4);
-		
-		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
-		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3, cidade4));
-		
-		Cliente cliente = new Cliente(null, "Maria Silva", "maria@gmail.com", "35378912377", TipoCliente.PESSOAFISICA);
-		
-		Endereco endereco1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cliente, cidade1);
-		Endereco endereco2 = new Endereco(null, "Avenida Matos", "105", null, "Centro", "38777012", cliente, cidade2);
-		
-		cliente.getEnderecos().addAll(Arrays.asList(endereco1, endereco2));
-		
-		cliente.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
-		
-		clienteRepository.save(cliente);
-		enderecoRepository.saveAll(Arrays.asList(endereco1, endereco2));
-		
-		Pedido pedido1 = new Pedido(null, LocalDateTime.now(), endereco1, cliente);
-		Pedido pedido2 = new Pedido(null, LocalDateTime.now(), endereco2, cliente);
-		
-		Pagamento pagamento1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, pedido1, 6);
-		pedido1.setPagamento(pagamento1);
-		
-		Pagamento pagamento2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, pedido2, LocalDateTime.now().plusMonths(1), null);
-		pedido2.setPagamento(pagamento2);
-		
-		cliente.getPedidos().addAll(Arrays.asList(pedido1, pedido2));
-		
-		pedidoRepository.saveAll(Arrays.asList(pedido1, pedido2));
-		pagamentoRepository.saveAll(Arrays.asList(pagamento1, pagamento2));
-		
-		ItemPedido itemPedido1 = new ItemPedido(pedido1, p1, 0.00, 1, 2000.00);
-		ItemPedido itemPedido2 = new ItemPedido(pedido1, p3, 0.00, 2, 80.00);
-		ItemPedido itemPedido3 = new ItemPedido(pedido2, p2, 100.00, 1, 800.00);
-		
-		pedido1.getItens().addAll(Arrays.asList(itemPedido1, itemPedido2));
-		pedido2.getItens().add(itemPedido3);
-		
-		p1.getItens().add(itemPedido1);
-		p2.getItens().add(itemPedido3);
-		p3.getItens().add(itemPedido2);
-		
-		itemPedidoRepository.saveAll(Arrays.asList(itemPedido1, itemPedido2, itemPedido3));
 		
 	}
 
