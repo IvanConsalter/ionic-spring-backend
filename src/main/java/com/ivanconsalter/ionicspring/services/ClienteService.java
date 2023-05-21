d657e5e680295eccd196492faa2ab2b8bd061664
Ivan Consalter <ivanconsalter1@gmail.com>
2023-05-21 17:11:13 -0300

    using pattern names for images

--- src/main/java/com/ivanconsalter/ionicspring/services/ClienteService.java
+++ src/main/java/com/ivanconsalter/ionicspring/services/ClienteService.java
@@ -1,11 +1,13 @@
 package com.ivanconsalter.ionicspring.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -47,6 +49,12 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 	
 	public List<ClienteDTO> findAll() {
 		List<Cliente> list = clienteRepository.findAll();
@@ -118,12 +126,9 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado");
 		}
 
-		URI uri = s3Service.uploadFile(multipartFile);
-
-		Cliente cli = findById(user.getId());
-		cli.setImageUrl(uri.toString());
-		clienteRepository.save(cli);
-
-		return uri;
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
+		
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 }
---
+++ src/main/java/com/ivanconsalter/ionicspring/services/ImageService.java
@@ -0,0 +1,67 @@
+package com.ivanconsalter.ionicspring.services;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ivanconsalter.ionicspring.services.exception.FileException;
+
+@Service
+public class ImageService {
+	
+	public BufferedImage getJpgImageFromFile(MultipartFile uploadedFile) {
+		String ext = this.getFileExtension(uploadedFile.getOriginalFilename());
+		if (!"png".equals(ext) && !"jpg".equals(ext)) {
+			throw new FileException("Somente imagens PNG e JPG são permitidas");
+		}
+
+		try {
+			BufferedImage img = ImageIO.read(uploadedFile.getInputStream());
+			if ("png".equals(ext)) {
+				img = pngToJpg(img);
+			}
+			return img;
+		} catch (IOException e) {
+			throw new FileException("Erro ao ler arquivo");
+		}
+	}
+
+	public BufferedImage pngToJpg(BufferedImage img) {
+		BufferedImage jpgImage = new BufferedImage(img.getWidth(), img.getHeight(),
+				BufferedImage.TYPE_INT_RGB);
+		jpgImage.createGraphics().drawImage(img, 0, 0, Color.WHITE, null);
+		return jpgImage;
+	}
+
+	public InputStream getInputStream(BufferedImage img, String extension) {
+		try {
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
+			ImageIO.write(img, extension, os);
+			return new ByteArrayInputStream(os.toByteArray());
+		} catch (IOException e) {
+			throw new FileException("Erro ao ler arquivo");
+		}
+	}
+	
+	private String getFileExtension(String filename) {
+		Path path = Paths.get(filename);
+		String file = path.getFileName().toString();
+		int dotIndex = file.lastIndexOf(".");
+		if(dotIndex == -1 || dotIndex == file.length() - 1) {
+			return "";
+		}
+		
+		return file.substring(dotIndex + 1);
+	}
+
+}
