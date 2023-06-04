e9846930055333f52bd1a6ad36f65b5e648cd294
Ivan Consalter <ivanconsalter1@gmail.com>
2023-06-04 17:13:04 -0300

    standardizing exception format

--- src/main/java/com/ivanconsalter/ionicspring/resources/exception/ValidationError.java
+++ src/main/java/com/ivanconsalter/ionicspring/resources/exception/ValidationError.java
@@ -9,19 +9,16 @@ public class ValidationError extends StandardError {
 	
 	private List<FieldMessage> errors = new ArrayList<>();
 
-	public ValidationError() {
+	public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+		super(timestamp, status, error, message, path);
 	}
 
-	public ValidationError(Integer status, String message, Long timeStamp) {
-		super(status, message, timeStamp);
-	}
-	
 	public List<FieldMessage> getErrors() {
 		return errors;
 	}
-	
-	public void addError(String fieldName, String message) {
-		errors.add(new FieldMessage(fieldName, message));
+
+	public void addError(String fieldName, String messagem) {
+		errors.add(new FieldMessage(fieldName, messagem));
 	}
 
 }
